f306dab585c87cfe48510e916d92a57d9d05814e
carbonellkatie <carbonellkatiecs@gmail.com>
2024-02-08 20:20:48 -0600

    xbox controller setup for determining button indexes

--- src/networking/networking.py
+++ src/networking/networking.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+from pygame.locals import *
 
 pygame.init()
 pygame.joystick.init()
@@ -6,7 +8,8 @@ pygame.joystick.init()
 num_joysticks = pygame.joystick.get_count()
 
 if num_joysticks > 0:
-    controller = pygame.joystick.Joystick(0)
+    # TODO: does not support multiple joysticks, make sure Joystick instance not created each time joystick is plugged back in
+    controller = pygame.joystick.Joystick(0) 
     controller.init()
     print("Controller connected:", controller.get_name())
 else:
@@ -19,3 +22,15 @@ while(running):
         # todo: may be unnecessary, provides way to shut down pygame
         if event.type == pygame.QUIT: 
             running = False
+            pygame.quit()
+        # button press down
+        if event.type == JOYBUTTONDOWN:
+            print("button pressed down")
+            print(event)
+            print("BUTTON PRESSED: " + str(event.button))
+        if event.type == JOYBUTTONUP:
+            print("button released")
+            print(event)
+        if event.type == JOYHATMOTION: 
+            print("hat button pressed")
+            print(event)
