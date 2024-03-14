3349cc49bc8c5cb700a4d1e70d61af26a68fc61a
braden624 <braden.j.white-1@ou.edu>
2024-03-14 13:53:55 -0500

    simplified video feed

--- src/operator/networking_operator.py
+++ src/operator/networking_operator.py
@@ -12,31 +12,21 @@ BUFF_SIZE = 65536
 
 def start():
 	
-    operator_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    operator_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-    host_name = socket.gethostname()
-    socket_address = ('', UDP_PORT)
-    operator_socket.bind(socket_address)
-    fps,st,frames_to_count,cnt = (0,0,20,0)
+    video_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    video_sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
+    socket_address = ('',UDP_PORT)
+    video_sock.bind(socket_address)   
+
     while True:
-          packet,_ = operator_socket.recvfrom(BUFF_SIZE)
+          packet,_ = video_sock.recvfrom(BUFF_SIZE)
           data = base64.b64decode(packet,' /')
-          npdata = np.fromstring(data, dtype=np.uint8)
-          frame = cv2.imdecode(npdata,1)
-          frame = cv2.putText(frame, 'FPS: ' + str(fps), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
-          cv2.imshow(frame)
+          parse = np.fromstring(data,dtype=np.uint8)
+          frame = cv2.imdecode(parse,1)
+          cv2.imshow("sender", frame)
           key = cv2.waitKey(1) & 0xFF
           if key == ord('q'):
-                operator_socket.close()
+                video_sock.close()
                 break
-          if cnt == frames_to_count:
-                try:
-                      fps = round(frames_to_count/(time.time()-st))
-                      st = time.time()
-                      cnt = 0
-                except:
-                      pass
-          cnt += 1      
 
 if __name__ == "__main__":
 	start()
--- src/robot/networking_robot.py
+++ src/robot/networking_robot.py
@@ -15,38 +15,16 @@ def start():
 
     video_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     video_sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-    host_name = socket.gethostname()
     socket_address = ('',UDP_PORT)
     video_sock.bind(socket_address)
-    video_stream(video_sock)
-    
-def video_stream(sock):
-
-    cap = cv2.VideoCamera(0)
-    fps,st,frames,cnt = (0,0,20,0)
 
     while True:
-        msg,client_addr = sock.recvfrom(BUFF_SIZE)
-        WIDTH = 400
-        while (cap.isOpened()):
-            _,frame = cap.read()
-            frame = imutils.resize(frame,width=WIDTH)
-            encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
-            message = base64.b64encode(buffer)
-            sock.sendto(message, client_addr)
-            frame = cv2.putText(frame,'FPS: ', + str(fps), (10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                sock.close()
-                break
-            if cnt == frames:
-                try:
-                    fps = round(frames/(time.time()-st))
-                    st = time.time()
-                    cnt = 0
-                except:
-                    pass
-            cnt+=1
-
+        vid = cv2.VideoCapture(0)
+        ret, frame = vid.read()
+        vid.release()
+        encoded, buff = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+        message = base64.b64encode(buff)
+        video_sock.sendto(message,socket_address)
+    
 if __name__ == "__main__":
     start()
