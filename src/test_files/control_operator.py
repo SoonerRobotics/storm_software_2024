265e3a440f31f2e50f770718fcc6f1022224573a
braden624 <braden.j.white-1@ou.edu>
2024-03-14 12:14:00 -0500

    Test scripts for controlling over sockets.

---
+++ src/test_files/control_operator.py
@@ -0,0 +1,50 @@
+import socket 
+import pygame
+import sys
+import glob
+import struct
+
+LEFT = 0
+RIGHT = 1
+
+SERVER_IP = "raspberrypi"
+SERVER_PORT = 12345
+
+def start():
+
+    pygame.init()
+    pygame.joystick.init()
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+
+    num_joysticks = pygame.joystick.get_count()
+    if num_joysticks > 0:
+        controller = pygame.joystick.Joystick(0)
+        controller.init()
+        print("Controller connected:", controller.get_name())
+    else:
+        print("No controller detected")
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.JOYAXISMOTION:
+                if event.axis == 1:
+                    left_y = -controller.get_axis(1)
+                    packet = LEFT.to_bytes(1,'little')
+                    packet = packet + bytearray(struct.pack("<f",left_y))
+                    server_socket.sendto(packet, (SERVER_IP, SERVER_PORT))
+                elif event.axis == 2:
+                    right_y = -controller.get_axis(3)
+                    packet = RIGHT.to_bytes(1,'little')
+                    packet = packet + bytearray(struct.pack("<f",right_y))
+                    server_socket.sendto(packet, (SERVER_IP, SERVER_PORT))
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    start()
---
+++ src/test_files/control_robot.py
@@ -0,0 +1,53 @@
+import socket
+import serial
+import sys
+import glob
+
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 12345
+
+def start():
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+
+    list_of_ports = serial_ports()
+    print("Please choose a connection port:")
+    port_range = range(len(list_of_ports))
+    for index, port in zip(port_range, list_of_ports):
+        print(index, port)
+    selection = input('Enter option number:')
+    if selection == 'q' or selection == 'Q':
+        sys.exit(0)
+    selected_port = list_of_ports[int(selection)-1]
+    pico = serial.Serial(port=selected_port, baudrate=112500)
+
+    while True:
+
+        data, address = server_socket.recvfrom(1024)
+        pico.write(data)
+
+
+def serial_ports():
+
+    ports = ["-----"]
+    if sys.platform.startswith('win'):
+        ports += ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        ports += glob.glob('/dev/tty[A-za-a]*')
+    elif sys.platform.startswith('darwin'):
+        ports += glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+if __name__ == "__main__":
+    start()
